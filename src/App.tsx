2500f16cb2e9dd1b4449847ab249c2ecafe545ff
Simon Senger <76154207+Eb0nheart@users.noreply.github.com>
2022-02-03 21:41:08 +0100

    format files with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import './App.css';
-import { Header } from './components/Header';
-import { ServiceContainer } from './components/ServiceContainer';
-import { GetServices} from "./scripts/backend";
+import "./App.css";
+import { Header } from "./components/Header";
+import { ServiceContainer } from "./components/ServiceContainer";
+import { GetServices } from "./scripts/backend";
 
 export const App = () => {
-
   return (
     <div className="App">
-      <Header/>
-      {GetServices().map(service => {
-        return (
-          <ServiceContainer service={service}/>
-        );
+      <Header />
+      {GetServices().map((service) => {
+        return <ServiceContainer service={service} />;
       })}
     </div>
   );
-}
+};
